aede6449693377d2a2e56beb6beec73060f09979
Dennis Garding <d.garding@shopware.com>
2024-05-18 15:34:10 +0200

    Add e2e tests

---
+++ playwright.config.js
@@ -0,0 +1,48 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 1,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://127.0.0.1:9001',
+    trace: 'on',
+    video: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npx esbuild --servedir=./ --serve=127.0.0.1:9001',
+    url: 'http://127.0.0.1:9001',
+    reuseExistingServer: !process.env.CI,
+  },
+});
+
---
+++ tests/multiselect.spec.js
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+test('Multiselect test', async ({ page }) => {
+    await test.step('Value attribute sets badges and highlights selected in list', async () => {
+        await page.goto('/html/multiselect.html');
+
+        const multiSelect = await page.locator('multi-select[name="multiSelect"]');
+        await expect(multiSelect).toBeVisible();
+
+        const badgeOne = await page.locator('.multi-select-selected-container > span[data-value="1"]');
+        await expect(badgeOne).toBeVisible();
+
+        const badgeFour = await page.locator('.multi-select-selected-container > span[data-value="4"]');
+        await expect(badgeFour).toBeVisible();
+
+        const input = await page.locator('input[class="multi-select-search-field-input"]');
+        await expect(input).toBeVisible();
+        await input.click();
+
+        const selectionList = await page.locator('.multi-select-select-list');
+        await expect(selectionList).toBeVisible();
+
+        const selectionOne = await page.locator('.multi-select-select-list > div[data-value="1"]');
+        await expect(selectionOne).toBeVisible();
+        await expect(selectionOne).toHaveClass(/selected/);
+
+        const selectionFour = await page.locator('.multi-select-select-list > div[data-value="4"]');
+        await expect(selectionFour).toBeVisible();
+        await expect(selectionFour).toHaveClass(/selected/);
+    });
+
+    await test.step('Removes badge and selected class on click current selected element', async () => {
+        const selectionOne = await page.locator('.multi-select-select-list > div[data-value="1"]');
+        await selectionOne.click();
+        await expect(selectionOne).not.toHaveClass(/selected/);
+
+        const badgeOne = await page.locator('.multi-select-selected-container > span[data-value="1"]');
+        await expect(badgeOne).not.toBeVisible();
+
+        const selectedContainer = await page.locator('.multi-select-selected-container');
+        await expect(selectedContainer).not.toHaveText('hallo');
+    });
+
+    await test.step('adds badge and selected class on click on not selected element', async () => {
+        const selectionTwo = await page.locator('.multi-select-select-list > div[data-value="2"]');
+        await selectionTwo.click();
+        await expect(selectionTwo).toHaveClass(/selected/);
+
+        const badgeTwo = await page.locator('.multi-select-selected-container > span[data-value="2"]');
+        await expect(badgeTwo).toBeVisible();
+        await expect(badgeTwo).toHaveText(/foo/);
+    });
+});
